baaf0ce22ccf48899b3f4dc07a4cf84990ca6cdc
Uj Levente <asdlevente@gmail.com>
2019-01-15 16:24:09 +0100

    repository set up

---
+++ src/main/java/com/codecool/jpaseriesminiproject/JpaSeriesMiniProjectApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.jpaseriesminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSeriesMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSeriesMiniProjectApplication.class, args);
+    }
+
+}
+
